7d3d6cce3627b98b8a72461e9f8d749a30b54e3f
Abhishek Jawalkar <jawalkarabhishek57@gmail.com>
2026-09-17 12:18:34 +0530

    Final Commit

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -8,6 +8,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
+import Github from './components/Github/Github.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements (
@@ -16,6 +17,7 @@ const router = createBrowserRouter(
       <Route path = 'about' element = {<About />} />
       <Route path = 'contact' element = {<Contact />} />
       <Route path = 'user/:userid' element = {<User />} />
+      <Route path = 'github' element = {<Github />} />
     </Route>
   )
 )
