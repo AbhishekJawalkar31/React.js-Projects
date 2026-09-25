df4233092e2fde8e79803fcac12bd808edfd0b15
Abhishek Jawalkar <jawalkarabhishek57@gmail.com>
2026-09-25 10:09:38 +0530

    Final commit

--- miniContext/src/App.jsx
+++ miniContext/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import UserContextProvider from './context/UserContextProvider'
+import Login from './components/login'
+import Profile from './components/Profile'
 
 function App() {
 
   return (
     <UserContextProvider>
       <h1>Context Api</h1>
+      <Login />
+      <Profile />
     </UserContextProvider>
   )
 }
